c259e5179eebb85569392f01558e9cd4662b678e
amishasacheti <amishajuly@gmail.com>
2021-10-03 20:11:02 +0530

    File Structure Changes and Added Sign Up Validators (#6)

---
+++ src/validators/SignUpValidator_Rules.js
@@ -0,0 +1,19 @@
+import isEmail from 'validator/lib/isEmail'
+export default {
+    type: {
+        email: ({ value }) => isEmail(value),
+        password: {
+            capitalLetter: ({ value }) => /[A-Z]/.test(value),
+            oneNumber: ({ value }) => /[0-9]/.test(value),
+            minLength: ({ value }) => value.length > 7
+        }
+    },
+    name: {
+        confirmPassword: {
+            matches:
+                ({ value, get }) => {
+                    return value === get(['userPassword', 'value'])
+                }
+        }
+    }
+}
